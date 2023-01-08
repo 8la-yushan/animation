2b1480bf79d890e8d044d77ea322cfcbc4a0df75
Lin <114201665+8la-yushan@users.noreply.github.com>
2023-01-08 23:01:28 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Animation Examples';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  double padValue = 200.0;
+  void _updatePadding(double value) {
+    setState(() {
+      padValue = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        AnimatedPadding(
+          padding: EdgeInsets.all(padValue),
+          duration: const Duration(seconds: 3),
+          curve: Curves.ease,
+          onEnd:() {
+            setState(() {
+              _updatePadding(padValue = 200.0);
+            });
+          },
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 5,
+            color: Colors.blue,
+          ),
+        ),
+
+        ElevatedButton(
+            child: const Text('✈flying'),
+            onPressed: () {
+              _updatePadding(padValue == 200.0 ? 0.0 : 200.0);
+            }),
+      ],
+    );
+  }
+}
